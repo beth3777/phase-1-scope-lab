66bed76645b81ab2778a7cb14cedd1253c19eced
email <bethkimaku@gmail.com>
2023-03-22 23:19:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName ='bob'
+
+function upperCaseCustomerName(){
+    var customerName=customerName.toUpperCase;
+    return (customerName);
+}
+function setBestCustomer(){
+    var bestCustomer ="not bob"
+    console.log (bestCustomer);
+}
+function overwriteBestCustomer(){
+    var bestCustomer ='bob'
+    console.log(bestCustomer);
+}
+
+const leastFavouriteCustomer ='jane'
+
+function changeLeastFavouriteCustomer(){
+    const leastFavouriteCustomer='joyce'
+    console.log (leastFavouriteCustomer)
+}
+leastFavouriteCustomer();
